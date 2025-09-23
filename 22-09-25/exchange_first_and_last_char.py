9bdd8de8533f96cd0316d628a57123b68532a494
Mohammed safvan. N <safvanmohammedn@gmail.com>
2025-09-23 16:29:44 +0530

    Delete 22-09-25 directory

--- 22-09-25/exchange_first_and_last_char.py
+++
@@ -1,7 +0,0 @@
-
-# ?)- create a string from given string where first and last charecters exchanged ?
-
-s = input("Enter a string: ")
-a= s[-1] + s[1:-1] + s[0]
-print(a)
-
--- 22-09-25/repace_with_$.py
+++
@@ -1,6 +0,0 @@
-
-# ?)- get a string from an input string where all occurences of first charecter replaced with doller symbol except first charecter ?
-
-s = input("Enter a string: ")
-a= s[0] + s[1: ].replace(s[0],"$")
-print(a)
